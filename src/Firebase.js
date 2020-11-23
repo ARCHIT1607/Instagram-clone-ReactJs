973edc8b1562c3035a4edc0d580143760c98e1b0
Archit1607 <Archit778@gmail.com>
2020-11-23 21:24:57 +0530

    cleaning project structure and adding firebase

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyB2z7dWJQZncK6lYWTU7DSMmTkzzCAk2eU",
+  authDomain: "instagram-clone-a8a6f.firebaseapp.com",
+  databaseURL: "https://instagram-clone-a8a6f.firebaseio.com",
+  projectId: "instagram-clone-a8a6f",
+  storageBucket: "instagram-clone-a8a6f.appspot.com",
+  messagingSenderId: "863201113594",
+  appId: "1:863201113594:web:31660e62c441c5b82da043",
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+export { firebaseApp, auth };
+export default db;
