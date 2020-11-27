274ad9101eed07c5cf71923b4ddde709eaff2a82
Archit1607 <Archit778@gmail.com>
2020-11-27 19:12:09 +0530

    modal with user auth using email and pwd is doe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
+import { Button, Input, makeStyles, Modal } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import "./App.css";
-import { db } from "./Firebase";
+import { auth, db } from "./Firebase";
 import Post from "./Post";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+
   const [posts, setPosts] = useState([]);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        // user has logged out
+        setUser(null);
+      }
+    });
+
+    return () => {
+      //perform some clean up atcion
+      unsubscribe();
+    };
+  }, [user, username]);
 
   useEffect(() => {
     //this is where the code runs
@@ -18,16 +67,140 @@ function App() {
     });
   }, []);
 
+  const signUp = (event) => {
+    event.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+    auth.signInWithEmailAndPassword(email, password).catch((err) => {
+      alert(err.message);
+    });
+    setOpenSignIn(false);
+  };
+
   return (
     <div className="app">
-      {/* Header */}
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1-DjUJ1UJd41jhB1K_jI7jMdVNUMSwp-_1g&usqp=CAU"
+                alt="logo"
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal
+        open={openSignIn}
+        onClose={() => {
+          openSignIn(false);
+        }}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1-DjUJ1UJd41jhB1K_jI7jMdVNUMSwp-_1g&usqp=CAU"
+                alt="logo"
+              />
+            </center>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
           alt=""
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1-DjUJ1UJd41jhB1K_jI7jMdVNUMSwp-_1g&usqp=CAU"
-        ></img>
+        />
       </div>
+
+      {user ? (
+        <Button
+          onClick={() => {
+            auth.signOut();
+          }}
+        >
+          Logout
+        </Button>
+      ) : (
+        <div className="app__loginContainer">
+          <Button
+            onClick={() => {
+              setOpenSignIn(true);
+            }}
+          >
+            Sign IN
+          </Button>
+          <Button
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            Sign Up
+          </Button>
+        </div>
+      )}
 
       {posts.map(({ id, post }) => (
         <Post
